3c042ead9ef98a6cf161f407e32bf8a012f2e31e
Mohammad Kermani <mkermani144@gmail.com>
2024-06-24 21:43:54 +0330

    feat: add `readMessage` script

---
+++ readMessage.js
@@ -0,0 +1,22 @@
+const { ethers } = require("hardhat");
+const box = require("ascii-box").box;
+
+const main = async () => {
+  const messageAddress = process.argv[2];
+
+  const TheMessage = await ethers.getContractFactory("TheMessage");
+  const theMessage = await TheMessage.attach(messageAddress);
+
+  const message = await theMessage.getMessage();
+  const timeSpent = await theMessage.getTimeSpent();
+
+  console.log("This is the message:\n");
+  console.log(box(message));
+  console.log(
+    `Around \n${
+      timeSpent / 1000n
+    } seconds of CPU time was spent to generate this address`
+  );
+};
+
+main();
