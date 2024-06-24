4df47edc4f4183ad8fa960761b3ea269f9de1fe3
Mohammad Kermani <mkermani144@gmail.com>
2024-06-24 21:30:14 +0330

    fix: make nonce bigint

--- findAddress.js
+++ findAddress.js
@@ -2,21 +2,22 @@ const ethers = require("ethers");
 
 const TheMessage = require("./artifacts/contracts/TheMessage.sol/TheMessage.json");
 
-let nonce = 0;
+let nonce = 0n;
 const started = Date.now();
 
 const factoryAddress = process.argv[2];
 const endsWith = process.argv[3];
+const message = process.argv[4];
 
 while (true) {
   const timeSpent = Date.now() - started;
   nonce++;
   const address = ethers.getCreate2Address(
     factoryAddress,
-    "0x" + (nonce + timeSpent).toString(16).padStart(64, "0"),
+    "0x" + (nonce + BigInt(timeSpent)).toString(16).padStart(64, "0"),
     ethers.keccak256(
       `${TheMessage.bytecode}${ethers.AbiCoder.defaultAbiCoder()
-        .encode(["string", "uint"], ["hello", timeSpent])
+        .encode(["string", "uint"], [message, timeSpent])
         .slice(2)}`
     )
   );
